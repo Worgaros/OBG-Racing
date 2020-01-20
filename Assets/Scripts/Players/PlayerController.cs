73c2e709c76df3317bf806461d7fd7e2b9c57c3a
William PEPIN <williamp10022000@gmail.com>
2020-01-20 19:02:39 +0100

    Add player movements in player controller script

---
+++ Assets/Scripts/Players/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D body;
+    
+    Vector2 direction;
+    [SerializeField] float speed;
+    [SerializeField] float inverse = -1;
+    [SerializeField] private bool isMoving = false;
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("ChangeDirection") && !isMoving)
+        {
+            direction = new Vector2( speed * inverse, speed);
+            isMoving = true;
+        }
+        else if (Input.GetButtonDown("ChangeDirection") && isMoving)
+        {
+            direction = new Vector2(body.velocity.x * inverse, body.velocity.y);
+        }
+        
+        
+        body.velocity = direction;
+    }
+}
