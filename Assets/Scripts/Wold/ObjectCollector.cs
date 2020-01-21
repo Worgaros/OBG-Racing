22c1ecc0dcfa919c9fa7c12cc69f7fe4db4a7ea2
William PEPIN <williamp10022000@gmail.com>
2020-01-21 15:14:59 +0100

    Add objects collector script

---
+++ Assets/Scripts/Wold/ObjectCollector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.CompareTag("Coin"))
+        {
+            Destroy(gameObject);
+        }
+        
+        else if (gameObject.CompareTag("Trap"))
+        {
+            Destroy(gameObject);
+        }
+        
+        else if (gameObject.CompareTag("Cup"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
